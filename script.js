e83266993c66f09f0617423a1dee3ae9461c86dd
Sriyan-Nakka <sriyannakka@gmail.com>
2025-05-30 11:43:33 +0530

    version 1.1

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 let css = "";
 let prevStyles = [];
+let markingNums = [];
 
 function generateTambolaTicket() {
+  markingNums = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
+    79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+  ];
+
+  //shuffling markingNums array
+  for (let i = 0; i < 100; i++) {
+    let a = Math.floor(Math.random() * markingNums.length);
+    let b = Math.floor(Math.random() * markingNums.length);
+
+    let temp = markingNums[a];
+    markingNums[a] = markingNums[b];
+    markingNums[b] = temp;
+  }
+
+  //marked number border reset
+  for (let i = 1; i <= 90; i++) {
+    document.getElementById(`num${i}`).style.color = "white";
+    document.getElementById(`num${i}`).style.borderColor = "white";
+  }
+
+  console.log(markingNums);
+
   prevStyles.forEach((s) => document.head.removeChild(s));
   prevStyles = [];
+
+  document.querySelector("#recentMarkedNum").textContent = "-";
+  document.getElementById("tambolaSpan").style.display = "none";
 
   for (let row = 1; row <= 3; row++) {
     for (let column = 1; column <= 9; column++) {
@@ -25,35 +55,15 @@ function generateTambolaTicket() {
     79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
   ];
 
-  document.getElementById(`r1c1`).textContent = " ";
-  document.getElementById(`r1c2`).textContent = " ";
-  document.getElementById(`r1c3`).textContent = " ";
-  document.getElementById(`r1c4`).textContent = " ";
-  document.getElementById(`r1c5`).textContent = " ";
-  document.getElementById(`r1c6`).textContent = " ";
-  document.getElementById(`r1c7`).textContent = " ";
-  document.getElementById(`r1c8`).textContent = " ";
-  document.getElementById(`r1c9`).textContent = " ";
-
-  document.getElementById(`r2c1`).textContent = " ";
-  document.getElementById(`r2c2`).textContent = " ";
-  document.getElementById(`r2c3`).textContent = " ";
-  document.getElementById(`r2c4`).textContent = " ";
-  document.getElementById(`r2c5`).textContent = " ";
-  document.getElementById(`r2c6`).textContent = " ";
-  document.getElementById(`r2c7`).textContent = " ";
-  document.getElementById(`r2c8`).textContent = " ";
-  document.getElementById(`r2c9`).textContent = " ";
-
-  document.getElementById(`r3c1`).textContent = " ";
-  document.getElementById(`r3c2`).textContent = " ";
-  document.getElementById(`r3c3`).textContent = " ";
-  document.getElementById(`r3c4`).textContent = " ";
-  document.getElementById(`r3c5`).textContent = " ";
-  document.getElementById(`r3c6`).textContent = " ";
-  document.getElementById(`r3c7`).textContent = " ";
-  document.getElementById(`r3c8`).textContent = " ";
-  document.getElementById(`r3c9`).textContent = " ";
+  for (let r1col = 1; r1col <= 9; r1col++) {
+    document.getElementById(`r1c${r1col}`).textContent = " ";
+  }
+  for (let r2col = 1; r2col <= 9; r2col++) {
+    document.getElementById(`r2c${r2col}`).textContent = " ";
+  }
+  for (let r3col = 1; r3col <= 9; r3col++) {
+    document.getElementById(`r3c${r3col}`).textContent = " ";
+  }
 
   document.querySelector("#ticketDiv").style.display = "block";
 
@@ -164,4 +174,28 @@ document.addEventListener("keyup", (e) => {
 
 function marked(event) {
   event.currentTarget.classList.toggle("marked");
+}
+
+let markNum = 0;
+
+function generateMarkingNum() {
+  document.getElementById("tambolaSpan").style.display = "inline-block";
+  document.getElementById(`num${markingNums[markNum]}`).style.color = "skyblue";
+  document.getElementById(`num${markingNums[markNum]}`).style.borderColor =
+    "blue";
+  document.querySelector("#recentMarkedNum").textContent = markingNums[markNum];
+  markNum++;
+  if (markNum >= markingNums.length) {
+    markNum = 0;
+    alert(
+      "All numbers have been called. A new Tambola Ticket will be generated when OK is pressed."
+    );
+    document.getElementById("tambolaSpan").style.display = "none";
+    document.querySelector("#recentMarkedNum").textContent = "-";
+    for (let i = 1; i <= 90; i++) {
+      document.getElementById(`num${i}`).style.color = "white";
+      document.getElementById(`num${i}`).style.borderColor = "white";
+    }
+    generateTambolaTicket();
+  }
 }
